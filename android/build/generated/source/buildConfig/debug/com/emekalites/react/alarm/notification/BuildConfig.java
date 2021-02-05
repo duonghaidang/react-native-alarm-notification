e5cf78bbe1995aa39cf57841bdb6ae9820704656
Kiet.Nguyen <kietnguyen@hitek.com.vn>
2021-02-05 21:19:29 +0700

    first commit

---
+++ android/build/generated/source/buildConfig/debug/com/emekalites/react/alarm/notification/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.emekalites.react.alarm.notification;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.emekalites.react.alarm.notification";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.emekalites.react.alarm.notification";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
